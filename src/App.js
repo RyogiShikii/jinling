dfce6e5335dcf504b909485e44d01389e7b4a21f
Daniel Guo <guosongraiden@gmail.com>
2019-06-03 16:00:24 -0700

    6.3 3rd update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 
 class App extends React.Component {
   render() {
-    const {Header, Footer} = Layout;
+    const {Footer} = Layout;
     return (
       <Router>
         <Layout>
